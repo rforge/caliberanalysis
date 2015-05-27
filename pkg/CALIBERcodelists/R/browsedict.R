7fa928fe7f11e1e32d7aec3bfcc8f8ea1043b359
anoopshah <anoopshah@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-27 13:13:30 +0000

    Corrections to CALIBERcodelists -- data in sysdata.R rather than an R object
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caliberanalysis@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CALIBERcodelists/R/browsedict.R
+++ pkg/CALIBERcodelists/R/browsedict.R
@@ -250,35 +250,6 @@ medcodeToReadCode <- function(medcodes){
 	out
 }
 
-importCSVcodelist <- function(filename, as.is=TRUE, ...){
-	# Imports a CSV codelist, automatically trying different delimiters
-	# if comma is incorrect. This function should only be used for importing
-	# codelists
-	temp <- data.table(read.csv(filename, as.is=as.is, ...))
-	
-	if (ncol(temp)==1){
-		# Only detected one column, try TAB delimiter
-		try(temp <- data.table(read.delim(filename,
-			sep='\t', as.is=as.is, ...)))
-	}
-	if (ncol(temp)==1){
-		# Only detected one column, try semicolon delimiter
-		try(temp <- data.table(read.delim(filename,
-			sep=' ', as.is=as.is, ...)))
-	}
-	if (ncol(temp)==1){
-		# Only detected one column, try space delimiter
-		try(temp <- data.table(read.delim(filename,
-			sep=' ', as.is=as.is, ...)))
-	}
-
-	if (ncol(temp)==1){
-		stop('Unable to load codelist, what I got was:\n' %&%
-			paste(show(temp), collapse='\n'))
-	}
-	return(temp)
-}
-
 
 browseLoad <- function(sprog='soffice'){
 	# Loads the selection, dictionary or section of CALIBER_DICT
