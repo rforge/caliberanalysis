457f7fc0b2dc77d8e3aa96e90a4e33b9f2500c7d
anoopshah <anoopshah@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-03-06 22:31:35 +0000

    CALIBERdrugdose testset and various corrections
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caliberanalysis@57 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/CALIBERdrugdose/data/testset.R
@@ -0,0 +1,5 @@
+# Test set of dosage instructions with intended output
+# Aim to test a wide variety of formats
+# Include any corrected analysis errors
+testset <- read.csv('testset.csv', header = TRUE, as.is = TRUE)
+
